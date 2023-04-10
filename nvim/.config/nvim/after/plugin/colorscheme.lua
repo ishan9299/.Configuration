140f16fdfc46a8d73e5d060a108ddbb0aec9188d
ishan <ishan365.ia@gmail.com>
2023-04-10 12:36:41 +0530

    remove nvim config

--- nvim/.config/nvim/after/plugin/colorscheme.lua
+++
@@ -1,4 +0,0 @@
-vim.cmd("packadd! nvim-solarized-lua")
-vim.cmd("packadd! minimal.nvim")
-vim.cmd("packadd! modus-theme-vim")
-vim.cmd("colorscheme modus-vivendi")
--- nvim/.config/nvim/after/plugin/mini.lua
+++
@@ -1,6 +0,0 @@
-vim.cmd("packadd! mini.nvim")
-require("mini.comment").setup({})
-require("mini.pairs").setup({})
-require("mini.tabline").setup({
-        show_icons = false
-})
